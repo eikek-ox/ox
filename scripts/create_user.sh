869838b08fa04252a7a9bfdd2c89fa4e0e1f5236
Eike <ek@eike-kohnert.de>
2019-01-01 12:54:04 +0100

    wip

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 source ../.env
+BACKEND_CONTAINER_NAME="${COMPOSE_PROJECT_NAME}_ox-backend_1
 echo "Please enter login data for new user account"
 read -e -p "IMAP server: " -i "$DEFAULT_IMAP_SERVER" IMAP_SERVER
 read -e -p "SMTP server: " -i "$DEFAULT_SMTP_SERVER" SMTP_SERVER
@@ -14,7 +15,7 @@ stty echo
 
 echo "Executing create user command in running ox-backend container..."
 #docker exec -i ox-backend -e PASSWORD="$PASSWORD" /opt/open-xchange/sbin/dockerox_create_user
-docker exec ox-backend /opt/open-xchange/sbin/createuser -c 1 -A oxadmin -P "$OX_ADMIN_PASSWORD" -u ek \
+docker exec $BACKEND_CONTAINER_NAME /opt/open-xchange/sbin/createuser -c 1 -A oxadmin -P "$OX_ADMIN_PASSWORD" -u ek \
 -d "Test User" -g Test -s User -p $PASSWORD -e "$EMAIL" \
 --imaplogin "$IMAP_LOGIN" --imapserver "$IMAP_SERVER" --smtpserver "$SMTP_SERVER"
 echo "Done."
